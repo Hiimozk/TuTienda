a48ecedce8342828b1cef56376786a595523f42c
Hiimozk <ozkarsad@gmail.com>
2021-08-16 10:25:09 -0300

    .

--- src/components/Body/ItemDetail.js
+++ src/components/Body/ItemDetail.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ItemCount from './ItemCount';
 import Button from 'react-bootstrap/Button';
 
 
 const ItemDetail = ({item}) => {
-    
+    const [unidades, setUnidades] = useState()
     const onAdd = (cantidad) => {
         console.log(cantidad)
-        const c = cantidad
-        return c
+        setUnidades(cantidad)
+        console.log(unidades)
 }
 
-    
+    if(unidades>0){
     return(
         <>
         <h1>Detalle Item</h1>
@@ -20,15 +20,25 @@ const ItemDetail = ({item}) => {
         <h3>{item.price}</h3>
         <img src={item.img}></img>
         <ItemCount stock={item.stock} initial={item.stock >= 1?1:0} onAdd={onAdd} />
-        <Button className="m-2" variant="success" disabled >Terminar compra</Button>
-        <h1>{}</h1>
+        <Button className="m-2" variant="success"  >Terminar compra</Button>
+        
         
         </>
-        
-        
-        
-
     )
+}else {
+    return (
+        <>
+        <h1>Detalle Item</h1>
+        <h1> {item.id} </h1>
+        <h2>{item.title}</h2>
+        <h3>{item.price}</h3>
+        <img src={item.img}></img>
+        <ItemCount stock={item.stock} initial={item.stock >= 1?1:0} onAdd={onAdd} />
+        
+        
+        </>
+    )
+}
 
 
 
